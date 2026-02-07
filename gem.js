a0b2ab2944d8a1e10cabfad5804f17c3d5f8c98c
Anindya Ganguly <anindyaganguly19@gmail.com>
2026-02-07 12:58:39 +0530

    Implement chat functionality with Gemini AI and fix import statement

--- gem.js
+++ gem.js
@@ -1,5 +1,41 @@
 import 'dotenv/config'
 import {GoogleGenerativeAI} from '@google/generative-ai'
-import * as readLine from 'node:readLine/promises'
+import * as readLine from 'node:readline/promises'
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_AI_KEY)
+const model = genAI.getGenerativeModel({
+    model:"gemini-3-flash",
+    systemInstruction:"You are a helpful Computer Science Engineer"
+})
+
+//interface to read your keyboard (stdin) and write to terminal (stdout)
+
+const rl = readLine.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+
+async function startChat(){
+    console.log("____Gemini chatbot started! (Type 'exit' to quit)_____")
+
+    while(true){
+        const userInput = await rl.question("You: ")
+
+        if(userInput.toLowerCase() === "exit"){
+            console.log("goodbye!!")
+            rl.close()
+            break
+        }
+
+        try{
+            const result = await model.generateContent(userInput)
+            console.log("Gemini's response: ")
+            const response = result.response.text()
+            console.log(response)
+        }
+        catch(err){
+            console.log(err.message)
+        }
+    }
+}
+startChat()
